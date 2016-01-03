5c19a5f7741ad2232114d39ccae2875cda032ee2
Matthew Smith <matthew.smith813@gmail.com>
2016-01-03 17:37:12 +0000

    initial commit adding shell project with SFML dependencies

---
+++ Gravity++/main.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Gravity++");
+	sf::CircleShape shape(100.f);
+
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
